bc8f42327cbb37861fa1bf9c3a08a9765c21191b
{ Walyson } <walysonmoura222@gmail.com>
2024-03-28 18:21:57 +0000

    new

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import fastifyCookie from "@fastify/cookie";
+
 import fastify from "fastify";
 
 export const app = fastify();
 
-app.register(fastifyCookie);
 
 app.get("/", async (req, reply) => {
   return reply.status(200).type("text/html").send("Hello World");
 });
+
+
