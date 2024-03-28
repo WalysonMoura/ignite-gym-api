64bd85fb264cedcb2c85e499c7817f328c31f651
{ Walyson } <walysonmoura222@gmail.com>
2024-03-28 19:03:14 +0000

    new

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,12 @@
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
-//import { } from "@fastify/swagger-ui";
+
 import { env } from "./env";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-const swaggerDocument = require("./docs/swagger.json");
 
 export const app = fastify();
 
 app.get("/", async (req, reply) => {
   return reply.status(200).type("text/html").send("Hello World");
-});
-
-app.register(fastifySwaggerUi, {
-  routePrefix: {
-    routePrefix: "/api-docs", // Your desired route prefix
-    staticCSP: '/*"', // This value is required
-  },
-  schema: swaggerDocument,
 });
 
 app.register(fastifyJwt, {
