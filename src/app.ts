c1da4ca2464f2ae7305b714b05f5adbc965cc3d2
{ Walyson } <walysonmoura222@gmail.com>
2024-03-28 18:53:51 +0000

    new

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
-
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
+//import { } from "@fastify/swagger-ui";
+import { env } from "./env";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+const swaggerDocument = require("./docs/swagger.json");
 
 export const app = fastify();
-
 
 app.get("/", async (req, reply) => {
   return reply.status(200).type("text/html").send("Hello World");
 });
 
+app.register(fastifySwaggerUi, {
+  routePrefix: {
+    routePrefix: "/api-docs", // Your desired route prefix
+    staticCSP: '/*"', // This value is required
+  },
+  schema: swaggerDocument,
+});
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: env.ACCESS_TOKEN_EXPIRATION,
+  },
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+});
--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   JWT_SECRET: z.string(),
+  ACCESS_TOKEN_EXPIRATION: z.string().default("10m"),
   PORT: z.coerce.number().default(3333),
 });
 
