907694790dca925afeec6d08add5b4029cdc7161
{ Walyson } <walysonmoura222@gmail.com>
2024-04-02 18:14:44 +0000

    new

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,15 @@
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
-import express from "express";
-import swaggerUI from "swagger-ui-express";
 
 import { env } from "./env";
 import swaggerDocument from "./lib/docs/swagger.json";
 import { userRoutes } from "./http/controller/user/routes";
 
-const swaggerExpress = express();
-
 export const app = fastify({ logger: true });
 
-/* app.get("/", async (req, reply) => {
+app.get("/", async (req, reply) => {
   return reply.status(200).type("text/html").send("Hello World");
-}); */
-
-swaggerExpress.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+});
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -27,10 +21,6 @@ app.register(fastifyJwt, {
     signed: false,
   },
 });
-
-
-
-
 
 //app.register(userRoutes);
 
