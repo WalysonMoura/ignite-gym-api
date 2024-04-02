6b7da2d7452eafa1080b85228f0b22d0d0e0e086
{ Walyson } <walysonmoura222@gmail.com>
2024-04-02 20:51:29 +0000

    new

--- src/http/controller/exercise/routes.ts
+++ src/http/controller/exercise/routes.ts
@@ -5,17 +5,29 @@ import path from "path";
 export async function exerciseRoutes(app: FastifyInstance) {
   //app.addHook("onRequest");
 
-  const demoExercisePath = path.resolve(__dirname, "..", "exercises", "gif");
-  
+  const demoExercisePath = path.resolve(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "..",
+    "public",
+    "gif"
+  );
+
   app.register(require("fastify-static"), {
     root: demoExercisePath,
     prefix: "/exercise/demo",
   });
 
+
+  
   const thumbExercisesPath = path.resolve(
     __dirname,
     "..",
-    "exercises",
+    "..",
+    "..",
+    "public",
     "thumb"
   );
 
