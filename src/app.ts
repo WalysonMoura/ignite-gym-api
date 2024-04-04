7200bc8da1a1e7bbf0f45be829e6d7709100357d
{ Walyson } <walysonmoura222@gmail.com>
2024-04-04 16:23:30 +0000

    new

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 
+import fastifySwagger from "@fastify/swagger";
+
 import { env } from "./env";
-import swaggerDocument from "./lib/docs/swagger.json";
-import { userRoutes } from "./http/controller/user/routes";
+
+const swagger = require("./lib/docs/swagger");
+
+//import { userRoutes } from "./http/controller/user/routes";
 
 export const app = fastify({ logger: true });
 
 app.get("/", async (req, reply) => {
   return reply.status(200).type("text/html").send("Hello World");
 });
+
+///app.register(fastifySwagger, {})
+////app.register(require('@fastify/swagger-ui'), {})
+
+app.register(fastifySwagger, swagger.options);
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -30,4 +39,11 @@ listeners.forEach((signal) => {
     await app.close();
     process.exit(0);
   });
+});
+app.register(require("@fastify/cors"), {
+  // put your options here
+  /**
+   *  origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:3000'],
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE']
+   */
 });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import { app } from "./app";
 import { env } from "./env";
 
-app.listen({
-  host: '0.0.0.0',
-  port:env.PORT,
-})
+async () => {
+  try {
+    await app
+      .listen({
+        host: "0.0.0.0",
+        port: 3001,
+      })
+      .then(() => {
+        console.log("🚀 HTTP Server Running!");
+      });
+
+    app.swagger();
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
