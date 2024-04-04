ff5bb2131aa5f42dcf6129dbb0b6ffeb05667b2a
{ Walyson } <walysonmoura222@gmail.com>
2024-04-04 16:37:27 +0000

    new

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 import { app } from "./app";
 import { env } from "./env";
 
-async () => {
-  try {
-    await app
-      .listen({
-        host: "0.0.0.0",
-        port: 3333,
-      })
-      .then(() => {
-        console.log("🚀 HTTP Server Running!");
-      });
 
+const startServer = async () => {
+  try {
+    await app.listen({
+      host: "0.0.0.0",
+      port: 3333,
+    });
+    console.log('🚀 HTTP Server Running!');
+    
     app.swagger();
   } catch (error) {
-    app.log.error(error);
+    console.error(error);
     process.exit(1);
   }
 };
+
+startServer();
